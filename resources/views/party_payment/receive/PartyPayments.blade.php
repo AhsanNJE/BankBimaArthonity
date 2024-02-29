02d88499da7852875a361e6fb5090566133a3f88
HASIBUR RAHAMAN <60450950+Hasib282@users.noreply.github.com>
2024-02-29 19:37:44 +0600

    Merge with development

--- resources/views/party_payment/receive/PartyPayments.blade.php
+++ resources/views/party_payment/receive/PartyPayments.blade.php
@@ -3,11 +3,26 @@
     <div class="add-search">
         <div class="row">
             <div class="col-md-3">
-                <button class="open-modal add" data-modal-id="addTransaction">Add Client Payment</button>
+                <button class="open-modal add" data-modal-id="addTransaction">Add Transaction Payment</button>
             </div>
-            <div class="col-md-9 search">
-                <input type="date" name="startDate" id="startDate" class="form-input">
-                <input type="date" name="endDate" id="endDate" class="form-input">
+            <div class="col-md-2">
+                <label for="startDate">Start Date</label>
+                <input type="date" name="startDate" id="startDate" class="form-input" value="{{ date('Y-m-d') }}">
+            </div>
+            <div class="col-md-2" >
+                <label for="endDate">End Date</label>
+                <input type="date" name="endDate" id="endDate" class="form-input" value="{{ date('Y-m-d') }}">
+            </div>
+            <div class="col-md-2">
+                <label for="searchOption">Search Option</label>
+                <select name="searchOption" id="searchOption">
+                    <option value="1">Transaction Id</option>
+                    <option value="2">Transaction With</option>
+                    <option value="3">User</option>
+                </select>
+            </div>
+            <div class="col-md-3">
+                <label for="search">Search Here</label>
                 <input type="text" name="search" id="search" class="form-input" placeholder="Search here...">
             </div>
         </div>
