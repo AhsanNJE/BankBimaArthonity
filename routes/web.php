b00024572caac641d085a15bc31fb3c1c2915f22
HASIBUR RAHAMAN <60450950+Hasib282@users.noreply.github.com>
2024-04-04 15:07:44 +0600

    copy attandence part

--- routes/web.php
+++ routes/web.php
@@ -406,15 +406,12 @@ Route::controller(ReportController::class)->group(function () {
 ///////////////////////////// PayRollController Controller ////////////////////////////////
 
 Route::controller(PayRollController::class)->group(function(){
-
     ///Attendence All Route 
     Route::get('/employee/attend/list','EmployeeAttendenceList')->name('employee.attend.list'); 
     Route::get('/add/employee/attend','AddEmployeeAttendence')->name('add.employee.attend'); 
-
-
-
-
-    
+    Route::post('/employee/attend/store','EmployeeAttendenceStore')->name('employee.attend.store'); 
+    Route::get('/edit/employee/attend/{date}','EditEmployeeAttendence')->name('employee.attend.edit'); 
+    Route::get('/view/employee/attend/{date}','ViewEmployeeAttendence')->name('employee.attend.view');
 
     // additional payroll crude routes
     Route::get('/show/payroll/setup','ShowPayrollSetup')->name('show.payroll.setup'); 
